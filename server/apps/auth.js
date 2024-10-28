19d3857715fd3f22f5766be3286a97d39d479ace
Martinesusu <tinnaphop.t@st.econ.tu.ac.th>
2024-10-28 21:13:02 +0700

    Auth Register & Login APIs

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,11 +1,83 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+    try {
+    const user = {
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName || null,
+        lastName: req.body.lastName || null,
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    const collection = db.collection("users");
+    await collection.insertOne(user);
+   
+    return res.json({
+        message: "User has been created successfully"
+    })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error creating new user",
+        });
+    }
+})
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
+authRouter.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Username and password are required",
+        });
+    }
+    
+    const user = await db.collection("users").findOne({ username });
+
+    if (!user) {
+         return res.status(404).json({
+              message: "User not found",
+         });
+    };
+
+    const isValidPassword = await bcrypt.compare(
+         password,
+         user.password
+    );
+
+    if (!isValidPassword) {
+         return res.status(400).json({
+              message: "Password not valid",
+         });
+    };
+
+    const token = jwt.sign(
+         { 
+              id: user._id, 
+              firstName: user.firstName, 
+              lastName: user.lastName 
+         },
+         process.env.SECRET_KEY,
+         {
+              expiresIn: "900000",
+         }
+    );
+
+    return res.json({
+         message: "Login successfully",
+         token,
+    });
+});
 
 export default authRouter;
